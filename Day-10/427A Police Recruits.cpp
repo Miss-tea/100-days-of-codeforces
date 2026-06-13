cdb3d534100f788409b1ce0d7f3ff29c001447ec
Miss-tea <lubainasakhawat@gmail.com>
2026-06-13 19:07:28 +0600

    day 15 1st problem

--- Day-10/427A Police Recruits.cpp
+++ Day-10/427A Police Recruits.cpp
@@ -4,26 +4,9 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-     int n ;
-     cin >> n ;
-     int cnt = 0 ;
-     
-     int x = 0 ;
-     for(int i = 0 ; i < n ; i++)
-     {
-        int a ;cin >> a;
-       if(a > 0) cnt = cnt +a ;
-       else if(a == -1 &&  cnt >0)
-       {
-        cnt--;
-       }
-       else if(a == -1 && cnt ==0)
-       {
-          x++;
-       }
-
-
-     }
-     cout << x ;
-    
+    int n , k , l , c,d,p,nl,np;
+    cin >>n>>k>>l>>c>>d>>p>>nl>>np;
+    int f = min(k*l/nl , c*d);
+    int q = min(f,np/p)    ;
+    cout <<q/n;
          }
